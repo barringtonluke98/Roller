6433d3efe946b43610c5e4ef08f194a80ae0b0b1
barringtonluke98 <barrington.luke98@gmail.com>
2017-02-03 12:21:24 -0500

    Update v1.1
    Condensed original script added normal dice rolling function and basic
    menu/os

--- Roller.py
+++ Roller.py
@@ -6,168 +6,38 @@ def ab_roller():
 	for i in range(7):
 		for s in range(4):
 			list[i-1].append(randint(1,6))
-	for i in range(100):
-		if list[0][0] < list[0][1]:
-			temp1 = list[0][0]
-			temp2 = list[0][1]
-			list[0][0] = temp2
-			list[0][1] = temp1
-		elif list[0][1] < list[0][2]:
-			temp1 = list[0][1]
-			temp2 = list[0][2]
-			list[0][1] = temp2
-			list[0][2] = temp1
-		elif list[0][2] < list[0][3]:
-			temp1 = list[0][2]
-			temp2 = list[0][3]
-			list[0][2] = temp2
-			list[0][3] = temp1
-	list[0][3] = 0
-	for i in range(100):
-		if list[1][0] < list[1][1]:
-			temp1 = list[1][0]
-			temp2 = list[1][1]
-			list[1][0] = temp2
-			list[1][1] = temp1
-		elif list[1][1] < list[1][2]:
-			temp1 = list[1][1]
-			temp2 = list[1][2]
-			list[1][1] = temp2
-			list[1][2] = temp1
-		elif list[1][2] < list[1][3]:
-			temp1 = list[1][2]
-			temp2 = list[1][3]
-			list[1][2] = temp2
-			list[1][3] = temp1
-	list[1][3] = 0
-	for i in range(100):
-		if list[2][0] < list[2][1]:
-			temp1 = list[2][0]
-			temp2 = list[2][1]
-			list[2][0] = temp2
-			list[2][1] = temp1
-		elif list[2][1] < list[2][2]:
-			temp1 = list[2][1]
-			temp2 = list[2][2]
-			list[2][1] = temp2
-			list[2][2] = temp1
-		elif list[2][2] < list[2][3]:
-			temp1 = list[2][2]
-			temp2 = list[2][3]
-			list[2][2] = temp2
-			list[2][3] = temp1
-	list[2][3] = 0
-	for i in range(100):
-		if list[3][0] < list[3][1]:
-			temp1 = list[3][0]
-			temp2 = list[3][1]
-			list[3][0] = temp2
-			list[3][1] = temp1
-		elif list[3][1] < list[3][2]:
-			temp1 = list[3][1]
-			temp2 = list[3][2]
-			list[3][1] = temp2
-			list[3][2] = temp1
-		elif list[3][2] < list[3][3]:
-			temp1 = list[3][2]
-			temp2 = list[3][3]
-			list[3][2] = temp2
-			list[3][3] = temp1
-	list[3][3] = 0
-	for i in range(100):
-		if list[4][0] < list[4][1]:
-			temp1 = list[4][0]
-			temp2 = list[4][1]
-			list[4][0] = temp2
-			list[4][1] = temp1
-		elif list[4][1] < list[4][2]:
-			temp1 = list[4][1]
-			temp2 = list[4][2]
-			list[4][1] = temp2
-			list[4][2] = temp1
-		elif list[4][2] < list[4][3]:
-			temp1 = list[4][2]
-			temp2 = list[4][3]
-			list[4][2] = temp2
-			list[4][3] = temp1
-	list[4][3] = 0
-	for i in range(100):
-		if list[5][0] < list[5][1]:
-			temp1 = list[5][0]
-			temp2 = list[5][1]
-			list[5][0] = temp2
-			list[5][1] = temp1
-		elif list[5][1] < list[5][2]:
-			temp1 = list[5][1]
-			temp2 = list[5][2]
-			list[5][1] = temp2
-			list[5][2] = temp1
-		elif list[5][2] < list[5][3]:
-			temp1 = list[5][2]
-			temp2 = list[5][3]
-			list[5][2] = temp2
-			list[5][3] = temp1
-	list[5][3] = 0
-	for i in range(100):
-		if list[6][0] < list[6][1]:
-			temp1 = list[6][0]
-			temp2 = list[6][1]
-			list[6][0] = temp2
-			list[6][1] = temp1
-		elif list[6][1] < list[6][2]:
-			temp1 = list[6][1]
-			temp2 = list[6][2]
-			list[6][1] = temp2
-			list[6][2] = temp1
-		elif list[6][2] < list[6][3]:
-			temp1 = list[6][2]
-			temp2 = list[6][3]
-			list[6][2] = temp2
-			list[6][3] = temp1
-	list[6][3] = 0
+	list = sorted(list, reverse=True)
 	list_rolled = [0,0,0,0,0,0,0]
 	for i in range(7):
-		for s in range(4):
+		for s in range(3):
 			list_rolled[i-1] += list[i-1][s-1]
-	for s in range(100):
-		if list_rolled[0] < list_rolled[1]:
-			temp1 = list_rolled[0]
-			temp2 = list_rolled[1]
-			list_rolled[0] = temp2
-			list_rolled[1] = temp1
-		elif list_rolled[1] < list_rolled[2]:
-			temp1 = list_rolled[1]
-			temp2 = list_rolled[2]
-			list_rolled[1] = temp2
-			list_rolled[2] = temp1
-		elif list_rolled[2] < list_rolled[3]:
-			temp1 = list_rolled[2]
-			temp2 = list_rolled[3]
-			list_rolled[2] = temp2
-			list_rolled[3] = temp1
-		elif list_rolled[3] < list_rolled[4]:
-			temp1 = list_rolled[3]
-			temp2 = list_rolled[4]
-			list_rolled[3] = temp2
-			list_rolled[4] = temp1
-		elif list_rolled[5] < list_rolled[6]:
-			temp1 = list_rolled[5]
-			temp2 = list_rolled[6]
-			list_rolled[5] = temp2
-			list_rolled[6] = temp1
 	list_rolled = sorted(list_rolled, reverse=True)
 	if list_rolled[5] <= 9:
 		re_roll()
 	else:
 		print "Your ability scores are: %s, %s, %s, %s, %s, %s" % (list_rolled[0], list_rolled[1], list_rolled[2], list_rolled[3], list_rolled[4], list_rolled[5])
-	print raw_input("Press enter to go again")
-	re_roll()
-ab_roller()
-
-
-	
-	
-	
-	
-	
-	
+	os()
+def os():
+	for i in range(20):
+		pc_in = raw_input("'A'bility score roller or 'D'ice roller: ").lower()
+		if pc_in == "d":
+			roller()
+		elif pc_in == "a":
+			ab_roller()
+		else:
+			print "I'm sorry that isn't an accepted input"
+def roller():
+	idx = input("How many dice? ")
+	xdi = input("How many sides per dice? ")
+	total = 0
+	for i in range(idx):
+		x = randint(1,xdi)
+		print x
+		total += x
+	print "The total is %s" % (total)
+	os()
+print "Welcome to my table top dice roller"
+print "At this point the system has been programmed to roll"
+print "the ability scores of Pathfinder and DnD based"
+print "characters along with a dice roller"
+os()
